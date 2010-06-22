5cf62f11b6ee8ebea96b36d0fdc373c37b760672
Juan David Gonzalez Cobas <dcobas@cern.ch>
2010-06-22 19:48:25 +0200

    vmodio: put together things that belong together
    Forward eclarations unnecessary, order of declarations was a mess.
    No logical changes involved, though

--- vmod/vmodio.c
+++ vmod/vmodio.c
@@ -11,8 +11,8 @@
 /*
  * this module is invoked as
  *     $ insmod vmodio lun=0,1,4 
- *		base_address=0x1200,0xA800,0x6000
- *		     irq=126,130,142
+ *			base_address=0x1200,0xA800,0x6000
+ *		     	irq=126,130,142
  */
 static int lun[MAX_DEVICES];
 static int nlun;
@@ -44,6 +44,43 @@ static int devices;
 /* Matrix for register the isr callback functions */
 static isrcb_t mezzanines_callback[MAX_DEVICES][VMODIO_SLOTS];
 
+/* obtain the irq of slot from a base_irq in slot 0 */
+static inline int vmodio_irq_shift(int base_irq, int slot)
+{
+	return base_irq - (0xe - (~(1<<slot) & 0xf));
+}
+
+static inline int upper_nibble_of(int byte)
+{
+	return 0xf & (byte >> 4);
+}
+
+static int interrupt_to_slot[] = {
+	-1, -1, -1, -1,
+	-1, -1, -1,  3,
+	-1, -1, -1,  2,
+	-1,  1,  0, -1,
+};
+
+/* get the slot an irq corresponds to */
+static inline int irq_to_slot(int tmp)
+{
+	return interrupt_to_slot[tmp&0xf];
+}
+
+/* get the device struct corresponding to a lun */
+static struct vmodio *lun_to_dev(int lun)
+{
+	int i = 0;
+
+	for (i = 0; i < devices; i++) {
+		struct vmodio *dev = &device_table[i];
+		if (dev->lun == lun)
+			return dev;
+	}
+	return NULL;
+}
+
 /* map vmodio VME address space */
 static struct pdparam_master param = {
       .iack   = 1,                /* no iack */
@@ -58,163 +95,16 @@ static struct pdparam_master param = {
 static unsigned long vmodio_map(unsigned long base_address)
 {
 	return find_controller(base_address, VMODIO_WINDOW_LENGTH,
-		VMODIO_ADDRESS_MODIFIER, 0, VMODIO_DATA_SIZE, &param);               
+		VMODIO_ADDRESS_MODIFIER, 0, VMODIO_DATA_SIZE, &param);
 }
 
-static int  vmodio_interrupt(void *irq_id);
-
-/* obtain the irq of slot from a base_irq in slot 0 */
-static inline int vmodio_irq_shift(int base_irq, int slot)
-{
-	return base_irq - (0xe - (~(1<<slot) & 0xf));
-}
-
-static inline int upper_nibble_of(int byte)
-{
-	return 0xf & (byte >> 4);
-}
-
-static int device_init(struct vmodio *dev, int lun, unsigned long base_address, int irq)
-{
-	int ret;
-	int i;
-
-	dev->lun	= lun;
-	dev->vme_addr	= base_address;
-	dev->vaddr	= vmodio_map(base_address);
-	dev->irq	= irq;
-
-	if (dev->vaddr == -1)
-		return -1;
-
-	/* Upper nibble of irq corresponds to a single VMODIO module */
-	irq_to_lun[upper_nibble_of(dev->irq)] = lun;
-
-	/* 
-	 * The irq corresponding to the first slot is passed as argument to the driver 
-	 * To the rest of slots, the irq is calculated by substracting constants 0, 1, 3, 7.
-	 */
-	for (i = 0; i < VMODIO_SLOTS; i++) {
-		int irq = vmodio_irq_shift(dev->irq, i);
-		slot_irq[lun][i] = irq;
-		ret = vme_request_irq(irq, vmodio_interrupt, &irq, "vmodio");
-		if(ret < 0){ 
-			printk(KERN_ERR PFX "Cannot register an irq to the device %d, error %d\n", 
-				  dev->lun, ret);
-			return -1;
-		}
-	}
-
-	return 0;
-}
-
-/* forward */
-static int get_address_space(
-	struct carrier_as *asp,
-	int board_number, int board_position, int address_space_number);
-
-static int register_isr(int (*isr_callback)(
-					 int board_number,
-                                        int board_position,
-                                        void* extra),
-                int board_number, int board_position);
-
-static int __init init(void)
-{
-	int device = 0;
-	int i;
-
-	printk(KERN_INFO PFX "initializing driver\n");
-
-	/* Initialize the needed matrix for IRQ */
-	for (i = 0; i < MAX_DEVICES; i++){
-		irq_to_lun[i] = -1;
-		slot_irq[i][0] = -1;
-		slot_irq[i][1] = -1;
-		slot_irq[i][2] = -1;
-		slot_irq[i][3] = -1;
-	}
-
-	if (nlun >= MAX_DEVICES) {
-		printk(KERN_ERR PFX "too many devices (%d)\n", nlun);
-		goto failed_init;
-	}
-	if (nlun != nbase_address || nlun != nirq) {
-		printk(KERN_ERR PFX
-			"Given %d luns but %d addresses and %d irqs\n",
-				nlun, nbase_address, nirq);
-		goto failed_init;
-	}
-
-	if (modulbus_carrier_register(DRIVER_NAME, get_address_space, register_isr) != 0) {
-		printk(KERN_ERR PFX "could not register %s module\n",
-			DRIVER_NAME);
-		goto failed_init;
-	}
-	printk(KERN_INFO PFX "registered as %s carrier\n", DRIVER_NAME);
-
-	devices = 0;
-	for (device = 0; device < nlun; device++){
-		struct vmodio *dev = &device_table[device];
-		
-		if (device_init(dev, lun[device], base_address[device], irq[device])) {
-			printk(KERN_ERR PFX "map failed! not configuring lun %d\n", 
-				dev->lun);
-			goto failed_register;
-		}
-		printk(KERN_INFO PFX "mapped at virtual address 0x%08lx\n",
-			dev->vaddr);
-		devices++;
-	}
-	printk(KERN_INFO PFX "%d devices configured\n", devices);
-
-	return 0;
-
-failed_register:
-	modulbus_carrier_unregister(DRIVER_NAME);
-failed_init:
-	printk(KERN_ERR PFX "failed to init, exit\n");
-	return -1;
-}
-
-static void __exit exit(void)
-{
-	int i, j;
-
-	printk(KERN_INFO PFX "uninstalling driver\n");
-	for(i = 0; i < MAX_DEVICES; i++)
-	for(j = 0; j < MAX_DEVICES; j++)
-		if(slot_irq[i][j] >= 0)
-			vme_free_irq(slot_irq[i][j]);
-	modulbus_carrier_unregister(DRIVER_NAME);
-}
-
-module_init(init);
-module_exit(exit);
-
-MODULE_AUTHOR("Juan David Gonzalez Cobas");
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("VMODIO driver");
-
-
+/* slot memory-mapped I/O offsets */
 static int vmodio_offsets[VMODIO_SLOTS] = {
 	VMODIO_SLOT0,
 	VMODIO_SLOT1,
 	VMODIO_SLOT2,
 	VMODIO_SLOT3,
 };
-
-static struct vmodio *lun_to_dev(int lun)
-{
-	int i = 0;
-
-	for (i = 0; i < devices; i++) {
-		struct vmodio *dev = &device_table[i];
-		if (dev->lun == lun)
-			return dev;
-	}
-	return NULL;
-}
 
 /**
  * @brief Get virtual address of a mezzanine board AS
@@ -308,18 +198,6 @@ static int register_isr(int (*isr_callback)(
         return 0;
 }
 
-static int interrupt_to_slot[] = {
-	-1, -1, -1, -1,
-	-1, -1, -1,  3,
-	-1, -1, -1,  2,
-	-1,  1,  0, -1,
-};
-
-static inline int irq_to_slot(int tmp)
-{
-	return interrupt_to_slot[tmp&0xf];
-}
-
 static int vmodio_interrupt(void *irq_id)
 {
 	int carrier_number = -1;
@@ -347,4 +225,115 @@ static int vmodio_interrupt(void *irq_id)
 
 	return IRQ_HANDLED;
 }
+
+static int device_init(struct vmodio *dev, int lun, unsigned long base_address, int irq)
+{
+	int ret;
+	int i;
+
+	dev->lun	= lun;
+	dev->vme_addr	= base_address;
+	dev->vaddr	= vmodio_map(base_address);
+	dev->irq	= irq;
+
+	if (dev->vaddr == -1)
+		return -1;
+
+	/* Upper nibble of irq corresponds to a single VMODIO module */
+	irq_to_lun[upper_nibble_of(dev->irq)] = lun;
+
+	/* 
+	 * The irq corresponding to the first slot is passed as argument to the driver 
+	 * To the rest of slots, the irq is calculated by substracting constants 0, 1, 3, 7.
+	 */
+	for (i = 0; i < VMODIO_SLOTS; i++) {
+		int irq = vmodio_irq_shift(dev->irq, i);
+		slot_irq[lun][i] = irq;
+		ret = vme_request_irq(irq, vmodio_interrupt, &irq, "vmodio");
+		if(ret < 0){ 
+			printk(KERN_ERR PFX "Cannot register an irq to the device %d, error %d\n", 
+				  dev->lun, ret);
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+static int __init init(void)
+{
+	int device = 0;
+	int i;
+
+	printk(KERN_INFO PFX "initializing driver\n");
+
+	/* Initialize the needed matrix for IRQ */
+	for (i = 0; i < MAX_DEVICES; i++){
+		irq_to_lun[i] = -1;
+		slot_irq[i][0] = -1;
+		slot_irq[i][1] = -1;
+		slot_irq[i][2] = -1;
+		slot_irq[i][3] = -1;
+	}
+
+	if (nlun >= MAX_DEVICES) {
+		printk(KERN_ERR PFX "too many devices (%d)\n", nlun);
+		goto failed_init;
+	}
+	if (nlun != nbase_address || nlun != nirq) {
+		printk(KERN_ERR PFX
+			"Given %d luns but %d addresses and %d irqs\n",
+				nlun, nbase_address, nirq);
+		goto failed_init;
+	}
+
+	if (modulbus_carrier_register(DRIVER_NAME, get_address_space, register_isr) != 0) {
+		printk(KERN_ERR PFX "could not register %s module\n",
+			DRIVER_NAME);
+		goto failed_init;
+	}
+	printk(KERN_INFO PFX "registered as %s carrier\n", DRIVER_NAME);
+
+	devices = 0;
+	for (device = 0; device < nlun; device++){
+		struct vmodio *dev = &device_table[device];
+		
+		if (device_init(dev, lun[device], base_address[device], irq[device])) {
+			printk(KERN_ERR PFX "map failed! not configuring lun %d\n", 
+				dev->lun);
+			goto failed_register;
+		}
+		printk(KERN_INFO PFX "mapped at virtual address 0x%08lx\n",
+			dev->vaddr);
+		devices++;
+	}
+	printk(KERN_INFO PFX "%d devices configured\n", devices);
+
+	return 0;
+
+failed_register:
+	modulbus_carrier_unregister(DRIVER_NAME);
+failed_init:
+	printk(KERN_ERR PFX "failed to init, exit\n");
+	return -1;
+}
+
+static void __exit exit(void)
+{
+	int i, j;
+
+	printk(KERN_INFO PFX "uninstalling driver\n");
+	for(i = 0; i < MAX_DEVICES; i++)
+	for(j = 0; j < MAX_DEVICES; j++)
+		if(slot_irq[i][j] >= 0)
+			vme_free_irq(slot_irq[i][j]);
+	modulbus_carrier_unregister(DRIVER_NAME);
+}
+
+module_init(init);
+module_exit(exit);
+
+MODULE_AUTHOR("Juan David Gonzalez Cobas");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("VMODIO driver");
 
