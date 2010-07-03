d267c48c8492f61d1ae3c3f198c9643e70839a26
Juan David Gonzalez Cobas <dcobas@cern.ch>
2010-07-03 19:51:14 +0200

    vmod12e16: interrupt-driven version

--- vmod/install.sh
+++ vmod/install.sh
@@ -1,4 +1,16 @@
 #!	/bin/sh
+for f in 12e16 mod-pci  modulbus_register ; do
+    if grep $f /proc/modules ; then
+        rmmod $f
+    fi
+done
+
+( cd L*/*/
 insmod modulbus_register.ko
-insmod vmodio.ko lun=0 base_address=0xa800 irq=126
-insmod vmodttl.ko lun=0 carrier=vmodio carrier_number=0 slot=3
+insmod mod-pci.ko  lun=1,0 bus_number=1,2 slot_number=14,13
+#insmod mod-pci.ko  lun=0 bus_number=1 slot_number=14
+insmod 12e16.ko lun=0 carrier=mod-pci carrier_number=1 slot=1  )
+
+DEVNO=`grep 12e16 /proc/devices | awk '{print $1}' `
+mknod /dev/vmod12e16.0 c $DEVNO 0
+
