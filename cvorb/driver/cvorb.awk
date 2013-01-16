6f141117fa1e93a049e45b12e139c96ec03cb697
Juan David Gonzalez Cobas <dcobas@cern.ch>
2013-01-16 10:46:18 +0100

    cvorb: add generic installation scripts
    Directly adapted from CBMIA's scripts, so all the crateconfig/PCI
    business has been removed for good

---
+++ cvorb/driver/cvorb.awk
@@ -0,0 +1,35 @@
+#
+# transfer2insmod.awk - extract insmod parameters from transfer.ref
+#
+# usage: transfer2insmod.awk DEVICE_NAME [transfer_file]
+#
+# e.g.:
+#  $ awk -f transfer2insmod.awk CVORB /acc/dsc/tst/cfv-864-cdv28/etc/transfer.ref
+#
+#  produces
+#     lun=1,2 base_address=0x400,0x800
+#
+
+BEGIN {
+	device_name = ARGV[1]
+	delete ARGV[1]
+	bcs = ""
+	slots = ""
+}
+
+/^#\+#/ && $6 == device_name  && $4 == "VME" {
+	# decode transfer.ref line
+	lun = lun "," $7
+	base_address = base_address "," $11
+}
+
+END {
+	insmod_params = " "
+
+	if (lun)
+		insmod_params = insmod_params "lun=" substr(lun, 2)
+	if (base_address)
+		insmod_params = insmod_params " base_address=" substr(base_address, 2)
+	print insmod_params
+}
+
---
+++ cvorb/driver/install_cvorb.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+DEVICE_NAME=CVORBN
+DRIVER_NAME=cvorb
+TRANSFER=/etc/transfer.ref
+
+OUTPUT=":"
+RUN=""
+
+while getopts hvnc:D:d:t: o
+do	case $o in
+	v)	OUTPUT="echo" ;;		# verbose
+	n)	RUN=":" ;;			# dry run
+	D)	DEVICE_NAME="$OPTARG" ;;
+	d)	DRIVER_NAME="$OPTARG" ;;
+	t)	TRANSFER="$OPTARG" ;;
+	[h?])	echo >&2 "usage: $0 [-?hvn] [-D device_name] [-d driver_name] [-c crateconfig]"
+		exit ;;
+	esac
+done
+
+$OUTPUT "Installing $DEVICE_NAME driver..."
+INSMOD_ARGS=`awk -f mil1553.awk $DEVICE_NAME $CRATECONFIG $TRANSFER`
+if [ x"$INSMOD_ARGS" == x"" ] ; then
+    echo "No $DEVICE_NAME declared in $TRANSFER, exiting"
+    exit 1
+fi
+
+INSMOD_CMD="/sbin/insmod $DRIVER_NAME.ko $INSMOD_ARGS"
+$OUTPUT "$DRIVER_NAME install by [$INSMOD_CMD]"
+sh -c "$RUN $INSMOD_CMD"
+
+MAJOR=`cat /proc/devices | awk '$2 == "'"$DRIVER_NAME"'" {print $1}'`
+if [ -z "$MAJOR" ]; then
+	echo "driver $DRIVER_NAME not installed!"
+	exit 1
+fi
