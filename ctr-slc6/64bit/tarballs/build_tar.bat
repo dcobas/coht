cb9a8166dcce971127e4983a9fa187b522e5907e
Julian Lewis <julian.lewis@cern.ch>
2013-04-08 11:22:26 +0200

    ctr: Forgot ctrinstall script for tarballs
    Signed-off-by: Julian Lewis <julian.lewis@cern.ch>
    Signed-off-by: Juan David Gonzalez Cobas <dcobas@cern.ch>

--- ctr-slc6/64bit/tarballs/build_tar.bat
+++ ctr-slc6/64bit/tarballs/build_tar.bat
@@ -3,5 +3,6 @@ echo Build tar file for ctrp delivery without cleaning
 
 cp makedir/Makefile ./tar_pci
 cp makedir/Kbuild ./tar_pci
+cp makedir/ctrinstall ./tar_pci
 cp ../driver/*.[ch] ./tar_pci
-tar -cjf tar_pci.tar.bz2 tar_pci/
+tar -cjf tar_pci.bz2 tar_pci/
