3147ba4e0a7a59e4887c643d48cf74bfe832ae2a
Juan David Gonzalez Cobas <dcobas@cern.ch>
2013-07-03 19:20:09 +0200

    vd80: rename to vd80-64
    This incompatible driver is the one and only that can will
    run in the final 64-bit platforms
    
    The installation scripts are modified so that invoking
    install_vd80.sh with the flag -6 will install the latest
    64-bit compatible driver. The -3 option is the default,
    and it installs the old (skel, noxml) driver

--- vd80-2/scripts/install_vd80.sh
+++ vd80-2/scripts/install_vd80.sh
@@ -3,25 +3,28 @@
 DEVICE_NAME=VD80
 TRANSFER=/etc/transfer.ref
 DRIVER_NAME=vd80
+PARSER=vd80.awk
 
 # Install the vd80 driver specific script
 
 OUTPUT=":"
 RUN=""
-while getopts hvnD:d:t: o
+while getopts hvn36D:d:t: o
 do	case $o in
 	v)	OUTPUT="echo" ;;		# verbose
 	n)	RUN=":" ;;			# dry run
+	3)	DRIVER_NAME=vd80 PARSER=vd80.awk ;;
+	6)	DRIVER_NAME=vd80-64 PARSER=vd80-64.awk ;;
 	D)	DEVICE_NAME="$OPTARG" ;;
 	d)	DRIVER_NAME="$OPTARG" ;;
 	t)	TRANSFER="$OPTARG" ;;
-	[h?])	echo >&2 "usage: $0 [-?hvnb] [-D device] [-d driver] [-t transfer]"
+	[h?])	echo >&2 "usage: $0 [-?hvnb36] [-D device] [-d driver] [-t transfer]"
 		exit ;;
 	esac
 done
 
 $OUTPUT "Installing $DEVICE_NAME driver..."
-INSMOD_ARGS=`awk -f vd80.awk $DEVICE_NAME $TRANSFER`
+INSMOD_ARGS=`awk -f $PARSER $DEVICE_NAME $TRANSFER`
 if [ x"$INSMOD_ARGS" == x"" ] ; then
     echo "No $DEVICE_NAME declared in $TRANSFER, exiting"
     exit 1
@@ -40,7 +43,6 @@ fi
 MINORS=`awk '/^#\+#/ && $6 == "'"$DEVICE_NAME"'" { printf("%s ", $7) }' $TRANSFER`
 $OUTPUT "creating device nodes for driver $DRIVER_NAME, major $MAJOR, minors $MINORS"
 for MINOR in $MINORS; do
-    NODE=`expr $MINOR + 1`
-    sh -c "$RUN rm -f /dev/$DRIVER_NAME.$NODE"
-    sh -c "$RUN mknod /dev/$DRIVER_NAME.$NODE c $MAJOR $NODE"
+    sh -c "$RUN rm -f /dev/$DRIVER_NAME.$MINOR"
+    sh -c "$RUN mknod -m 777 /dev/$DRIVER_NAME.$MINOR c $MAJOR $MINOR"
 done
---
+++ vd80-2/scripts/vd80-64.awk
@@ -0,0 +1,49 @@
+#
+# vd80-64.awk - extract insmod parameters from transfer.ref
+#
+# usage: vd80.awk DEVICE_NAME [transfer_file]
+#
+# e.g.:
+#  $ awk -f vd80.awk VD80 /acc/dsc/tst/cfv-864-cdv28/etc/transfer.ref
+#
+#  produces
+#     insmod vd80.ko dname=vd80 luns=1,2,3
+#                               vme1=0x980000,0xa00000,0xa80000
+#                               vme2=0x980000,0xa00000,0xa80000
+#                               vecs=0xB0,0xB1,0xB2
+#
+
+BEGIN	{
+	device_name = ARGV[1]
+	delete ARGV[1]
+	luns = ""
+	base_address1 = ""
+	base_address2 = ""
+	vectors = ""
+	slots= ""
+}
+
+/^#\+#/ && $6 == device_name  && $4 == "VME" {
+	# decode transfer.ref line
+	luns =  luns "," $7
+	base_address2 =  base_address2 "," "0x" $16
+	vectors =  vectors "," $23
+	slots = slots "," $20
+	base_address1 = sprintf("%s,0x%08x", base_address1, $20 * 0x80000)
+}
+
+END {
+
+	insmod_params = " "
+
+	if (luns) insmod_params = insmod_params "luns=" substr(luns, 2)
+
+	if (vectors) insmod_params = insmod_params " vecs=" substr(vectors, 2)
+
+	if (slots) insmod_params = insmod_params " slot=" substr(slots, 2)
+
+	if (base_address2) insmod_params = insmod_params " vmeb=" substr(base_address2, 2)
+
+	print insmod_params
+
+}
